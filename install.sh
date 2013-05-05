5581c677bdf4b388c0962151bd9212ed7bb0284e
eloycoto <eloy.coto@gmail.com>
2013-05-05 11:23:57 +0100

    Add a lot of plugins, use all with pathogen. Copy a few from evim but in this case generate new projects.
    projects.

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DATE=$(date +%Y%m%d)
+DIR=$(pwd)
+
+#Vims conf
+if [ -d ~/.vim ]
+then
+    mv ~/.vim ~/.vim-$DATE
+fi
+
+if [ -f ~/.vimrc ]
+then
+    mv ~/.vimrc ~/.vimrc-$DATE
+fi
+
+rm -rfv $HOME/.vim
+cp -Rv $DIR/vim/ $HOME/.vim/
+
+rm  $HOME/.vimrc
+rm  $HOME/.gvimrc
+ln -s  $HOME/.vim/vimrc $HOME/.vimrc
+ln -s  $HOME/.vim/vimrc $HOME/.gvimrc
