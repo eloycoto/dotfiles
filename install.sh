e380825bdbfedeae546bf912f451ad241e8c827a
Eloy Coto <eloy.coto@gmail.com>
2018-01-10 18:48:34 +0100

    Switch to Plug in VIM

--- install.sh
+++ install.sh
@@ -4,47 +4,17 @@ DATE=$(date +%Y%m%d)
 DIR=$(pwd)
 ZSHRC=1
 
-git submodule init
-git submodule update
-
 
 #Vims conf
-if [ -d ~/.vim ]
-then
-    mv ~/.vim ~/.vim-$DATE
-fi
-
-if [ -f ~/.vimrc ]
-then
-    mv ~/.vimrc ~/.vimrc-$DATE
-fi
-
-if [ -f ~/.ctags ]
-then
-    mv ~/.ctags ~/.ctags-$DATE
-fi
-
 cp -v ctags ~/.ctags
-
-if [ -f ~/.dev_requirements.txt ]
-then
-    mv ~/.dev_requirements.txt ~/.dev_requirements.txt-$DATE
-fi
-cp -v dev_requirements.txt ~/.dev_requirements.txt
-
-#Move tmux config to home folder
+rm -rf $HOME/.vim
+cp -Rv $DIR/vim/ $HOME/.vim/
+ln -sf  $HOME/.vim/vimrc $HOME/.vimrc
 cp -v tmux.conf ~/.tmux.conf
 cp -v psqlrc ~/.psqlrc
 cp -rfv psqlrc_alias ~/.psqlrc_alias
 cp -Rfv bin ~/bin
-
-
-rm -rfv $HOME/.vim
-cp -Rv $DIR/vim/ $HOME/.vim/
-
-rm  $HOME/.vimrc
-ln -s  $HOME/.vim/vimrc $HOME/.vimrc
-
+cp -v dev_requirements.txt ~/.dev_requirements.txt
 
 cd /tmp/
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
@@ -59,11 +29,6 @@ cd $DIR
 cp -Rv zshrc $HOME/.zshrc
 cp -Rv bash_aliases $HOME/.bash_aliases
 cp -Rv ssh/* $HOME/.ssh/
-
-if [ -d ~/.gitconfig ]
-then
-    mv ~/.gitconfig ~/.gitconfig-$DATE
-fi
 cp -v gitconfig ~/.gitconfig
 
 sudo pip install virtualenvwrapper
